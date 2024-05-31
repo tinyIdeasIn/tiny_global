43ecbd22f907bf4ec91c74ac5d12f9bb31aba2a0
10456 <exscall@addcn.com>
2024-05-31 11:39:46 +0800

    init project

---
+++ lib/tiny_global.dart
@@ -0,0 +1,7 @@
+library tiny_global;
+
+export 'src/app_info.dart';
+export 'src/auth_manager.dart';
+export 'src/global_notification.dart';
+export 'src/open_manager.dart';
+export 'src/regex_manager.dart';
